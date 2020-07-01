1800611480e35e40144eb0dfb6131c8986b4059e
SBarton2005 <sbarton2024@k12.andoverma.us>
2020-07-01 15:02:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+import turtle
+import math
+t = turtle.Turtle()
+def base(width, height, color):
+  t.down()
+  t.fillcolor(color)
+  t.begin_fill()
+  t.forward(width)
+  t.left(90)
+  t.forward(height)
+  t.left(90)
+  t.forward(width)
+  t.left(90)
+  t.forward(height)
+  t.left(90)
+  t.end_fill()
+  t.up()
+def roof(hh, width, height, color):
+  leg = width / 2
+  hyp = (leg ** 2 + height ** 2) ** .5
+  a = height ** 2 + hyp ** 2 - leg ** 2
+  b = 2 * height * hyp
+  angle1 = math.degrees(math.acos(a/b))
+  c = leg ** 2 + hyp ** 2 - height ** 2
+  d = 2 * leg * hyp
+  angle2 = 90-angle1
+  angle1 *= 2
+  t.left(90)
+  t.forward(hh)
+  t.fillcolor(color)
+  t.begin_fill
+  t.right(90)
+  t.forward(width)
+  t.right(angle2)
+  t.down()
+  t.backward(hyp)
+  t.right(angle1)
+  t.forward(hyp)
+  t.end_fill()
+  t.up()
+  #This is wrong, find the right angle to bring back to the start
+  t.left(angle2)
+  t.forward(hh)
+  t.left(90)
+def house():
+  number = int(input("# of Houses: "))
+  i = 0
+  t.up()
+  while i < number:
+    #Here goes the program
+    #Getting house parts.
+    chimney = input("Chimney (y/n): ")
+    circle = input("Attic Window (y/n): ")
+    square = input("House Window (y/n): ")
+    land = input("Front Land (y/n): ")
+    #Getting Variables
+    width = int(input("House Width: "))
+    height = int(input("House Height: "))
+    color = input("House Color: ")
+    rheight = int(input("Roof Height: "))
+    rcolor = input("Roof Color: ")
+    dwidth = int(input("Door Width: "))
+    dheight = int(input("Door Height: "))
+    dcolor = input("Door Color: ")
+    if chimney == "y":
+      cwidth = int(input("Chimney Width: "))
+      cheight = int(input("Chimney Height: "))
+      ccolor = input("Chimney Color: ")
+    if circle == "y":
+      wradius = int(input("Window Radius: "))
+      wcolor = input("Window Color: ")
+    if square == "y":
+      sside = int(input("Window Side: "))
+      scolor = input("Window Color: ")
+    if land == "y":
+      ldistance = int(input("Land Distance: "))
+      lcolor = input("Land Color: ")
+    t.backward(290)
+    t.right(90)
+    t.forward(150)
+    t.left(90)
+    #Running the programs
+    base(width, height, color)
+    roof(height, width, rheight, rcolor)
+    #This is the end
+    i += 1
+house()
